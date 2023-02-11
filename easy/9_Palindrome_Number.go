067da7915c5810d2c83cf9009f2b8c6d387c904b
Dawei Fu <fudw@pku.edu.cn>
2023-02-11 13:28:08 +0800

    init

---
+++ easy/9_Palindrome_Number.go
@@ -0,0 +1,14 @@
+package goLeetCode
+
+import "strconv"
+
+func isPalindrome(x int) bool {
+	s := strconv.Itoa(x)
+	length := len(s)
+	for i := 0; i <= length/2; i++ {
+		if s[i] != s[length-1-i] {
+			return false
+		}
+	}
+	return true
+}
