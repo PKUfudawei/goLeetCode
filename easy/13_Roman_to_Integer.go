9e9844163f74eae3e79e9878e166e1312ef4f75c
Dawei Fu <fudw@pku.edu.cn>
2023-02-15 01:43:01 +0800

    easy 13

---
+++ easy/13_Roman_to_Integer.go
@@ -0,0 +1,28 @@
+package goLeetCode
+
+func romanToInt(s string) int {
+	roman := map[string]int{
+		"I": 1,
+		"V": 5,
+		"X": 10,
+		"L": 50,
+		"C": 100,
+		"D": 500,
+		"M": 1000,
+	}
+	if s == "" {
+		return 0
+	}
+	num, lastint, total := 0, 0, 0
+	for i := 0; i < len(s); i++ {
+		char := s[len(s)-(i+1) : len(s)-i]
+		num = roman[char]
+		if num < lastint {
+			total = total - num
+		} else {
+			total = total + num
+		}
+		lastint = num
+	}
+	return total
+}
