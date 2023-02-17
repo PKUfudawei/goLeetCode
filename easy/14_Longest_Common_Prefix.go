aaafa21ecd51095027248253f6f134a08ddd2c60
Dawei Fu <fudw@pku.edu.cn>
2023-02-17 16:49:59 +0800

    easy 0014

--- easy/14_Longest_Common_Prefix.go
+++ easy/14_Longest_Common_Prefix.go
@@ -3,10 +3,10 @@ package goLeetCode
 func LongestCommonPrefix(strs []string) string {
 	common_prefix := strs[0]
 
-	for _, str := range strs {
-		for j := 0; j < len(common_prefix); j++ {
-			if j+1 > len(str) || str[j] != common_prefix[j] {
-				common_prefix = common_prefix[:j]
+	for i := 0; i < len(common_prefix); i++ {
+		for _, str := range strs {
+			if i+1 > len(str) || str[i] != common_prefix[i] {
+				common_prefix = common_prefix[:i]
 				break
 			}
 		}
