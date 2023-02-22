46210cc70ed12f178b6681ebf2ca22db6f32b531
Dawei Fu <fudw@pku.edu.cn>
2023-02-22 20:13:08 +0800

    0222

--- easy/88. Merge Sorted Array.go
+++ easy/88. Merge Sorted Array.go
@@ -1,5 +1,15 @@
 package goLeetCode
 
 func Merge(nums1 []int, m int, nums2 []int, n int) {
+	i, j := 0, 0
 
+	for order := i + j; j < n; order++ {
+		if i < m && nums1[m-1-i] > nums2[n-1-j] {
+			nums1[m+n-1-order] = nums1[m-1-i]
+			i++
+		} else {
+			nums1[m+n-1-order] = nums2[n-1-j]
+			j++
+		}
+	}
 }
